9a652dd3612b0a4d74e5908b005fef8560dda4c3
Juan Pablo Abara <jpabara1@gmail.com>
2024-09-23 20:26:59 -0300

    Added 3 scenes

--- web/js/scenes.js
+++ web/js/scenes.js
@@ -1,18 +1,23 @@
 var scenes = [
     {
-        fileName: 'nakba-cities.geojson',
-        type: 'geojson',
-        layerType: 'symbol',
-        layerProperties: {
-            'layout': {
-                'icon-image': 'mapbox-home'
-            }
-        },
-        activePropertiesOnHover: [{
-            'Village (En)': 'Nombre',
-            'District (En)': 'Distrito',
-            'pop 1948': 'Población en 1948',
-            'jewish settlements on built area post-1948': 'Asentamientos judíos construídos después de 1948',
+        sceneName: 'Localidades palestinas destruidas en 1948',
+        sceneDescription: 'Muestra todas las localidades palestinas destruidas en 1948, incluyendo el número de habitantes en 1948 y los asentamientos judíos construidos en su lugar.',
+        data: [{
+            fileName: 'nakba-cities.geojson',
+            name: 'Localidades palestinas destruidas en 1948',
+            type: 'geojson',
+            layerType: 'symbol',
+            layerProperties: {
+                'layout': {
+                    'icon-image': 'mapbox-home'
+                }
+            },
+            activePropertiesOnHover: [{
+                'Village (En)': 'Nombre',
+                'District (En)': 'Distrito',
+                'pop 1948': 'Población en 1948',
+                'jewish settlements on built area post-1948': 'Asentamientos judíos construídos después de 1948',
+            }],
         }],
         center: [35.112757175425116, 31.903940273167674],
         pitch: 30,
@@ -20,24 +25,88 @@ var scenes = [
         zoom: 8,
     },
     {
-        fileName: 'nakba-cities.geojson',
-        type: 'geojson',
-        layerType: 'symbol',
-        layerProperties: {
-            'layout': {
-                'icon-image': 'mapbox-grocery'
-            }
-        },
-        activePropertiesOnHover: [{
-            'Village (En)': 'Nombre',
-            'District (En)': 'Distrito',
-            'pop 1948': 'Población en 1948',
-            'jewish settlements on built area post-1948': 'Asentamientos judíos construídos después de 1948',
+        sceneName: 'Asentamientos ilegales',
+        sceneDescription: 'Muestra los asentamientos ilegales en Cisjordania y Jerusalén Este, así como los asentamientos ilegales en Gaza. Los asentamientos ilegales están en violación del derecho internacional.',
+        data: [{
+            fileName: 'settlements_point.geojson',
+            name: 'Asentamientos ilegales',
+            type: 'geojson',
+            layerType: 'symbol',
+            layerProperties: {
+                'layout': {
+                    'icon-image': 'mapbox-triangle'
+                }
+            },
+            activePropertiesOnHover: [{
+                'name_es': 'Nombre',
+                'date_estab': 'Fecha de establecimiento',
+                'settlement': 'Tipo'
+            }],
         }],
-        center: [35.112757175425116, 31.903940273167674],
-        pitch: 60,
+        center: [34.60079940233604, 31.174435128776484],
+        pitch: 70,
         bearing: 30,
-        zoom: 6,
-    }
+        zoom: 10,
+        end: {
+            center: [34.987071229161835, 31.804467072754665],
+            pitch: 70,
+            bearing: 30,
+            zoom: 10,
+            duration: 4000,
+        }
+    },
+    {
+        sceneName: 'Zonas A, B y C',
+        sceneDescription: 'Las zonas A, B y C son áreas de Cisjordania que fueron establecidas en el Acuerdo Interino de Oslo I y II. La zona A está bajo control civil y de seguridad palestino, la zona B está bajo control civil palestino y seguridad israelí y la zona C está bajo control civil y de seguridad israelí. Israel ha aprovechado las zonas B y C para construir asentamientos ilegales de manera indiscriminada.',
+        data: [
+            {
+                fileName: 'area_a.geojson',
+                name: 'Zona A',
+                type: 'geojson',
+                layerType: 'fill',
+                layerProperties: {
+                    "paint": {
+                        "fill-color": "rgba(0, 255, 0, 0.5)"
+                    }
+                },
+                activePropertiesOnHover: [{
+                    'name_es': 'Nombre',
+                }],
+            },
+            {
+                fileName: 'area_b.geojson',
+                name: 'Zona B',
+                type: 'geojson',
+                layerType: 'fill',
+                layerProperties: {
+                    "paint": {
+                        "fill-color": "rgba(0, 0, 255, 0.5)"
+                    }
+                },
+                activePropertiesOnHover: [{
+                    'name_es': 'Nombre',
+                }],
+            },
+            {
+                fileName: 'area_c.geojson',
+                name: 'Zona C',
+                type: 'geojson',
+                layerType: 'fill',
+                layerProperties: {
+                    "paint": {
+                        "fill-color": "rgba(255, 0, 0, 0.5)"
+                    }
+                },
+                activePropertiesOnHover: [{
+                    'name_es': 'Nombre',
+                }],
+            }
+        ],
+        center: [35.09216936125758, 31.899221509220492],
+        pitch: 20,
+        bearing: 0,
+        zoom: 8,
+    },
+    
 ]
 
--- web/js/scenesLoader.js
+++ web/js/scenesLoader.js
@@ -2,6 +2,12 @@ const nextScene = () => {
     console.log("Moving to next scene.")
     previousSceneNr = sceneNr
     sceneNr += 1
+    if (sceneNr >= scenes.length) {
+        sceneNr = 0
+    }
+    if (sceneNr < 0) {
+        sceneNr = 0
+    }
     renderScene(sceneNr)
 }
 
@@ -9,12 +15,16 @@ const previousScene = () => {
     console.log("Moving to previous scene.")
     previousSceneNr = sceneNr
     sceneNr -= 1
+    if (sceneNr >= scenes.length) {
+        sceneNr = 0
+    }
+    if (sceneNr < 0) {
+        sceneNr = 0
+    }
     renderScene(sceneNr)
 }
 
-const cleanScene = (sceneNr) => {
-    const sourceName = `source-scene-${sceneNr}`
-    const layerName = `layer-scene-${sceneNr}`
+const removeLayerAndSource = (layerName, sourceName) => {
     if (map.getLayer(layerName)) {
         console.log("Removing layer: ", layerName)
         map.removeLayer(layerName)
@@ -25,53 +35,115 @@ const cleanScene = (sceneNr) => {
     }
 }
 
+const cleanScene = (sceneNr) => {
+    const sourceBaseName = `source-scene-${sceneNr}`
+    const layerBaseName = `layer-scene-${sceneNr}`
+    if (!scenes[sceneNr]) return
+    for (const [i, sceneData] of scenes[sceneNr].data.entries()) {
+        let sceneSourceName = `${sourceBaseName}_${i}`
+        let sceneLayerName = `${layerBaseName}_${i}`
+        removeLayerAndSource(sceneLayerName, sceneSourceName)
+    }
+    const legend = document.getElementById('legend')
+    legend.innerHTML = ''
+}
+
+const addToLegend = (layerProperties, layerName) => {
+    const legend = document.getElementById('legend')
+    const legendItem = document.createElement('div')
+    let colorHTML = ''
+    if (layerProperties.paint) {
+        colorHTML = `<span class="legend-color" style="background-color: ${layerProperties.paint['fill-color']}"></span>`
+    }
+    legendItem.innerHTML = `<div class="legend-item">
+        <div class="legend-text">${colorHTML} ${layerName}</div>
+        
+    </div>`
+    legend.appendChild(legendItem)
+}
+
+const loadSceneDescription = (sceneConfig) => {
+    const sceneDescription = document.getElementById('scene-description')
+    let descriptionHTML = ''
+    if (sceneConfig.sceneDescription) {
+        descriptionHTML = `<p id="scene-text">${sceneConfig.sceneDescription}</p>`
+    }
+    sceneDescription.innerHTML = `
+        <h2 id="scene-title">${sceneConfig.sceneName}</h2>
+        ${descriptionHTML}
+    `
+}
+
 const renderScene = (sceneNr) => {
     cleanScene(previousSceneNr)
     const sceneConfig = scenes[sceneNr]
-    const sourceName = `source-scene-${sceneNr}`
-    const layerName = `layer-scene-${sceneNr}`
+    const sourceBaseName = `source-scene-${sceneNr}`
+    const layerBaseName = `layer-scene-${sceneNr}`
 
-    map.addSource(sourceName, {
-        type: 'geojson',
-        data: `data/${sceneConfig.fileName}`
-    });
-
-    map.addLayer({
-        ...sceneConfig.layerProperties,
-        'id': layerName,
-        'type': sceneConfig.layerType,
-        'source': sourceName,
-    });
+    for (const [i, sceneData] of sceneConfig.data.entries()) {
+        let sceneSourceName = `${sourceBaseName}_${i}`
+        let sceneLayerName = `${layerBaseName}_${i}`
+        map.addSource(sceneSourceName, {
+            type: 'geojson',
+            data: `data/${sceneData.fileName}`
+        });
+        map.addLayer({
+            ...sceneData.layerProperties,
+            'id': sceneLayerName,
+            'type': sceneData.layerType,
+            'source': sceneSourceName,
+        });
+        addToLegend(
+            sceneData.layerProperties,
+            sceneData.name,
+        )
+    }
+    loadSceneDescription(sceneConfig)
 
     map.flyTo({
         center: sceneConfig.center || initialCenter,
         pitch: sceneConfig.pitch || 0,
         bearing: sceneConfig.bearing || 0,
         zoom: sceneConfig.zoom || initialZoom,
+        duration: 2000,
         essential: true
     });
+
+    if (sceneConfig.end) {
+        setTimeout(() => {
+            map.flyTo({
+                center: sceneConfig.end.center || initialCenter,
+                pitch: sceneConfig.end.pitch || 0,
+                bearing: sceneConfig.end.bearing || 0,
+                zoom: sceneConfig.end.zoom || initialZoom,
+                duration: sceneConfig.end.duration || 1,
+                essential: true,
+            });
+        }, 2000);
+    }
 
     map.on('mouseenter', layerName, (e) => {
         map.getCanvas().style.cursor = 'pointer';
 
         const coordinates = e.features[0].geometry.coordinates.slice();
         let description = ''
-
-        for (const activeProperty of scenes[sceneNr].activePropertiesOnHover) {
-            for (const key in activeProperty) {
-                if (e.features[0].properties[key]) {
-                    description += `<strong>${activeProperty[key]}:</strong> ${e.features[0].properties[key]}<br>`
+        const sceneConfig = scenes[sceneNr]
+        for (const sceneData of sceneConfig.data) {
+            for (const activeProperty of sceneData.activePropertiesOnHover) {
+                for (const key in activeProperty) {
+                    if (e.features[0].properties[key]) {
+                        description += `<strong>${activeProperty[key]}:</strong> ${e.features[0].properties[key]}<br>`
+                    }
                 }
             }
-        }
 
-        if (['mercator', 'equirectangular'].includes(map.getProjection().name)) {
-            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            if (['mercator', 'equirectangular'].includes(map.getProjection().name)) {
+                while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+                }
             }
+            popup.setLngLat(coordinates).setHTML(description).addTo(map);
         }
-
-        popup.setLngLat(coordinates).setHTML(description).addTo(map);
     });
 
     map.on('mouseleave', layerName, () => {
