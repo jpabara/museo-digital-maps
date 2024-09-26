21cfd4156700a71eebae9fad63c6b212a80b2dde
Juan Pablo Abara <jpabara1@gmail.com>
2024-09-26 19:24:51 -0300

    Fixed progress bar

--- web/js/progressBar.js
+++ web/js/progressBar.js
@@ -1,12 +1,4 @@
 const setProgress = async (sceneNr) => {
-    const progressBar = document.getElementById('progress-bar')
-    for (let i = 0; i < 1000; i++) {
-        await setTimeout(() => {
-            progressBar.value = (sceneNr - 1) + easeInOutQuint(i/1000)
-        }, 100);
-    }
-}
-
-function easeInOutQuint(x) {
-    return x < 0.5 ? 16 * x * x * x * x * x : 1 - Math.pow(-2 * x + 2, 5) / 2;
+    const progressBar = document.getElementById('progress-bar-fill')
+    progressBar.style.width = `${(sceneNr/(scenes.length-1))*100}%`
 }
