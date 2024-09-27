6b3019dc60caa26450efc2102f06f3c0d5c90acd
Juan Pablo Abara <jpabara1@gmail.com>
2024-09-27 16:37:08 -0300

    Changed maptok

--- web/js/initializeMap.js
+++ web/js/initializeMap.js
@@ -2,7 +2,7 @@ var mapId = 'map'
 var mapStyle = 'mapbox://styles/joempa/cm1a2q73i00i001nw839t333q'
 var initialCenter = [35.00856210932468, 31.84378912291933]
 var initialZoom = 7
-var mapboxAccessToken = 'pk.eyJ1Ijoiam9lbXBhIiwiYSI6ImNtMWZyaGR0eDNjbnEyaW9qYWFiN285ZHMifQ.H9MwmkBQxxHLr-kj2_4LIw'
+var mapboxAccessToken = 'pk.eyJ1Ijoiam9lbXBhIiwiYSI6ImNtMWZyZm42YjF6ZGIycXBwdWN2MGY1bzUifQ.vgMUiOkC-kp4kpuJkBuvtg'
 var sceneNr = 0
 var previousSceneNr = -1
 var map
